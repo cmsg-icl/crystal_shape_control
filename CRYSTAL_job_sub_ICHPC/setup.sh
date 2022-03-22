1fcde7be1e29bb0d49c7834fb24b60d1f4aa7c17
Spica-Vir <61191919+Spica-Vir@users.noreply.github.com>
2022-03-22 00:10:04 +0000

    version 0.2.2 bug fix
    'checkfile' script in post_processing can get properly generated
    
    the old 'setfile' command changed to 'setcrys' to avoid ambiguity

--- CRYSTAL_job_sub_ICHPC/setup.sh
+++ CRYSTAL_job_sub_ICHPC/setup.sh
@@ -18,7 +18,7 @@ function welcome_msg {
 CRYSTAL17 job submitter for Imperial HPC - Setting up
 
 Job submitter installed at: `date`
-Job submitter edition:      v0.2.1
+Job submitter edition:      v0.2.2
 Supported job scheduler:    PBS
 
 By Spica-Vir, Mar. 18-22, ICL, spica.h.zhou@gmail.com
@@ -189,12 +189,12 @@ function set_commands {
     sed -i '/CRYSTAL job submitter settings/d' ${HOME}/.bashrc
     sed -i '/Pcry=/d' ${HOME}/.bashrc
     sed -i '/Pprop=/d' ${HOME}/.bashrc
-    sed -i '/setfile=/d' ${HOME}/.bashrc
+    sed -i '/setcrys=/d' ${HOME}/.bashrc
 
     echo "# >>> CRYSTAL job submitter settings >>>" >> ${HOME}/.bashrc
     echo "alias Pcry='${SCRIPTDIR}/gen_sub crys'" >> ${HOME}/.bashrc
     echo "alias Pprop='${SCRIPTDIR}/gen_sub prop'" >> ${HOME}/.bashrc
-    echo "alias setfile='cat ${SCRIPTDIR}/settings'" >> ${HOME}/.bashrc
+    echo "alias setcrys='cat ${SCRIPTDIR}/settings'" >> ${HOME}/.bashrc
     echo "# <<< finish CRYSTAL job submitter settings <<<" >> ${HOME}/.bashrc
 
     source ${HOME}/.bashrc
@@ -221,7 +221,7 @@ function set_commands {
         jobname:  str, name of input .d3 file
         SCFname:  str, optional, name of the previous 'crystal' job
 
-    setfile - print the file 'settings'
+    setcrys - print the file 'settings'
 	
 ================================================================================
 EOF
